9d12196d8b2fb28af6eacdbb9c63e2cc6a91ca8e
ErickDM157 <erickdambros449@gmail.com>
2025-02-16 21:43:09 -0400

    Implementação do css das telas de configuração de conta e fizalização da tela de duas configurações

---
+++ Public/js/conta.js
@@ -0,0 +1,17 @@
+function toggleVisibility() {
+    let elementos = document.querySelectorAll(".bg-img"); // Seleciona todos os elementos com a classe .bg-img
+    elementos.forEach((elemento) => {
+        if (window.matchMedia("(max-width: 700px)").matches) {
+            elemento.style.display = "none";
+        } else {
+            elemento.style.display = "block"; // Volta a exibir quando for maior que 800px
+        }
+    });
+}
+
+// Executa quando a tela é redimensionada
+window.addEventListener("resize", toggleVisibility);
+
+// Executa quando a página carrega
+toggleVisibility();
+
